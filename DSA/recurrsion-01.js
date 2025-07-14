50ef3bd2dec25c75bebe5f300b9a0b61000cf4f1
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2025-07-14 20:37:59 +0530

    print from 1 to n using recurrsion

--- DSA/recurrsion-01.js
+++ DSA/recurrsion-01.js
@@ -1,10 +1,24 @@
 
+//print from n to 1 using recurrsion
 
-function print(n){
-    if(n < 1) return ;
-    console.log(n);
-    print(--n)//n=n-1
-              //print(n)
-}
-n= 10;
-print(n)
+// function print(n){
+//     if(n < 1) return ;
+//     console.log(n);
+//     print(--n)//n=n-1
+//               //print(n)
+// }
+// n= 10;
+// print(n)
+
+
+//print 1 to n using recurrsion 
+// let n = 4;
+// function print(x){
+//     if(x > n) return ;
+//     console.log(x);
+//     print(++x)
+// }
+// print(1)
+// let n = 5;
+
+
