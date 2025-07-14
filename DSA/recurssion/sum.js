7abde8cc4c50753d8ad9465957556b70701754ca
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2025-07-14 21:56:47 +0530

    tansfer from recurrsion folder

--- DSA/recurssion/sum.js
+++ DSA/recurssion/sum.js
@@ -1,9 +1,9 @@
 //print the SUM OF FIRST N numbers using RECURRSION
-// function sum(n){
-//     if( n == 0) return 0;//base case
-//     return n + sum(n-1);
-// }
-//  console.log(sum(5))//15
+function sum(n){
+    if( n == 0) return 0;//base case
+    return n + sum(n-1);
+}
+ console.log(sum(5))//15
 
  //find the summ of all the elements in an array using recurrsion
 
