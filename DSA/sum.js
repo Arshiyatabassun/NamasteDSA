33f7c9bf7674d4d5214f9f7254c88695ff2d927f
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2025-07-14 21:52:12 +0530

    summ of all elements in an ARRAY using recurrsion

---
+++ DSA/sum.js
@@ -0,0 +1,16 @@
+//print the SUM OF FIRST N numbers using RECURRSION
+// function sum(n){
+//     if( n == 0) return 0;//base case
+//     return n + sum(n-1);
+// }
+//  console.log(sum(5))//15
+
+ //find the summ of all the elements in an array using recurrsion
+
+ let arr = [5,3,2,0,1]
+ function findSum(n){
+    if( n == 0) return arr[n];
+    return arr[n]  + findSum(n-1)
+
+ }
+ console.log(findSum(arr.length-1))
