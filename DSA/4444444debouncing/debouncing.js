75063ebddc82822fb3bde8d69038daa5a78512c3
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2025-08-29 21:22:40 +0530

    debounce

---
+++ DSA/4444444debouncing/debouncing.js
@@ -0,0 +1,19 @@
+let counter =0;
+const getData =()=>{
+console.log("Fetching data",counter)
+}
+const doSomthing = (fn,d)=>{
+   let timer;
+    return ()=>{
+ let context =this;
+ let args =arguments;
+ 
+    clearTimeout(timer)
+    timer = setTimeout(()=>{
+ fn.apply(context,arguments)
+    },d)
+       
+    }
+}
+
+const betterFunction =doSomthing(getData,300)
