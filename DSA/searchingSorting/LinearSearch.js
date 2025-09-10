feb7581ec4cb04d2c0067e071f44988cf1e65b7e
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2025-09-10 21:18:12 +0530

    binarysearch

--- DSA/searchingSorting/LinearSearch.js
+++ DSA/searchingSorting/LinearSearch.js
@@ -4,10 +4,10 @@
 function linearSearch(arr,target){
     for(let i= 0;i <arr.length;i++){
         if(arr[i] === target){
-            return i;
+            return i;//if target value is present then return the index of tht value
         }
     }
-    return -1;
+    return -1;//if no element present it returns -1
 }
 
 let arr =[2,3,4,6,8,9,0];
