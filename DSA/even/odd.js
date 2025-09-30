e2c834299277ddbe0a648c713631adf6b60bf5df
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2025-09-30 21:03:10 +0530

    sum of n numbers

--- DSA/even/odd.js
+++ DSA/even/odd.js
@@ -287,3 +287,12 @@
 // }
 // let  n =10;
 // printOneTon(1)
+
+//sum of first N numbers
+
+function sumOfN(num){
+    
+    if(num === 0) return 0;
+    return  num + sumOfN(num-1)
+}
+console.log(sumOfN(6))
