4da9770e8b831bd7a44357195b6c2f7bdd1175ab
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2026-03-09 16:11:03 +0530

    searchelement practise

--- DSA/loops1.js
+++ DSA/loops1.js
@@ -1,12 +1,28 @@
 
 
-function searchElement(arr,x){
-    for(let i =0 ;i <arr.length;i++){
-        if(arr[i] == x){
-            return i;
-        }
+// function searchElement(arr,x){
+//     for(let i =0 ;i <arr.length;i++){
+//         if(arr[i] == x){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+// let arr =[1,2,3,4,5];
+// console.log(searchElement(arr,4))
+
+//practice
+function searchElement(arr ,x){
+
+    for(let i = 0;i < arr.length;i++){
+      if(arr[i] === x)
+            {
+                return i;
+            }
     }
-    return -1;
+      return -1;
+ 
+
 }
-let arr =[1,2,3,4,5];
+let  arr =[1,2,4,5,9,8];
 console.log(searchElement(arr,4))
